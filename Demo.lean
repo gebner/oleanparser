25b91c3ba1b3bbe74c8544150204d4ebc122cdbc
Gabriel Ebner <gebner@gebner.org>
2021-12-30 19:12:13 +0100

    Add demo script.

---
+++ Demo.lean
@@ -0,0 +1,23 @@
+import Oleanparser
+import Lean
+open Lean
+
+structure Any where
+  α : Type
+  a : α
+
+instance : Inhabited Any := ⟨_, ()⟩
+
+instance : Coe α Any := ⟨(⟨_, ·⟩)⟩
+
+unsafe def testData : Array Any :=
+  #[123, "123", #[1,2,3], [1,2,3,4].toByteArray,
+    (-1 : Int), (3.14 : Float),
+    (Thunk.mk fun () => 123),
+    (Task.spawn fun () => 123),
+    unsafeBaseIO (IO.mkRef ())]
+
+-- TODO: add API to serialize olean files in-memory
+#eval saveModuleData "Demo.notolean" `Demo (unsafeCast testData)
+
+#eval parseOLean.run <$> IO.FS.readBinFile "Demo.notolean"
