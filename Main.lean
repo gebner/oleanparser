0a5cf7d7acdb73a74c30b43cdf2ac0e6501a6db7
Sebastian Ullrich <sebasti@nullri.ch>
2023-04-01 22:56:30 +0200

    Add --env-stats.

--- Main.lean
+++ Main.lean
@@ -12,19 +12,49 @@ def String.leftpad (n : Nat) (s : String) : String :=
 def ppNum (n : Nat) : String :=
   toString n |>.toList.reverse.toChunks 3 |>.map (·.reverse |> .mk) |>.intersperse "." |>.reverse |> .join
 
+partial def reifyName : Obj → Lean.Name
+  | .scalar 0 => .anonymous
+  | .ctor 1 #[n, .string s] _ => .str (reifyName n) s
+  | .ctor 2 #[n, .scalar p] _ => .num (reifyName n) p
+  | _ => unreachable!
+
+def Lean.HashMap.inc (m : Lean.HashMap String Nat) (s : String) (amount := 1) :=
+  m.insert s (m.findD s 0 + amount)
+
+def Obj.toArray! : Obj → Array Obj
+  | .array a => a
+  | _ => unreachable!
+
+def Lean.Name.getString? : Name → Option String
+  | .str _ s => s
+  | _ => none
+
 unsafe def runCmd (p : Parsed) : IO UInt32 := do
   let mut cumStats := {}
+  let mut reachableObjsPerCat : Lean.HashMap String Nat := ∅
   for file in p.variableArgsAs! String do
     let cnt ← IO.FS.readBinFile file
     let ((obj, st), _) ← ofExcept <| parseOLean.run cnt cumStats
     if p.hasFlag "dump" then
       println <| repr obj
     cumStats := st.stats
+    if p.hasFlag "env-stats" then
+      if let .ctor 0 #[_imports, constNames, constants, _extraConstNames, entries] _ := obj then
+        let cstage2Refs := StateT.run' (m := Id) (s := ∅) do
+          for (name, c) in constNames.toArray!.zip constants.toArray! do
+            let name := reifyName name
+            if name.getString?.any (·.startsWith "_cstage") || name.components.any (·.getString?.any (·.startsWith "_spec")) then
+              c.countRefsCore
+          get
+        reachableObjsPerCat := reachableObjsPerCat.inc "old compiler constants" cstage2Refs.size
+        for e in entries.toArray! do
+          let .ctor 0 #[extName, extEntries] _ := e | unreachable!
+          let crefs := extEntries.countRefs.size - 1  -- ignore containing array
+          if crefs > 1 then
+            reachableObjsPerCat := reachableObjsPerCat.inc (reifyName extName |> toString) crefs
   if p.hasFlag "stats" then
     let cumBytes := cumStats.bytesPerTag.toArray.foldl (· + ·.2) 0
     let cumObjs := cumStats.objsPerTag.toArray.foldl (· + ·.2) 0
-    let ppRelAbs n m (suffix := "") :=
-      s!"{ppNum n |>.leftpad 12}{suffix} ({(n * 100 + 50) / m |> ppNum |>.leftpad 4}%)"
     let printStats name objs bytes := do
       if objs > 0 then
         println s!"{name}\t{ppRelAbs objs cumObjs}\t{ppRelAbs bytes cumBytes "B"}"
@@ -43,7 +73,16 @@ unsafe def runCmd (p : Parsed) : IO UInt32 := do
     println ""
     let sharedPointers := cumStats.numPointers - cumObjs
     println s!"shared pointers\t{ppRelAbs sharedPointers cumStats.numPointers}"
+    println ""
+  unless reachableObjsPerCat.isEmpty do
+    let cumObjs := cumStats.objsPerTag.toArray.foldl (· + ·.2) 0
+    println "objects reachable from"
+    for (name, num) in reachableObjsPerCat.toArray.qsort (·.2 > ·.2) do
+      println s!"{ppRelAbs num cumObjs} {name}"
   return 0
+where
+  ppRelAbs n m (suffix := "") :=
+    s!"{ppNum n |>.leftpad 12}{suffix} ({(n * 100 + 50) / m |> ppNum |>.leftpad 4}%)"
 
 unsafe def cmd : Cmd := `[Cli|
   oleanparser VIA runCmd;
@@ -52,6 +91,7 @@ unsafe def cmd : Cmd := `[Cli|
   FLAGS:
     dump;  "Prints pretty-printed object trees."
     stats; "Prints object metadata statistics."
+    "env-stats"; "Prints statistics about `Lean.Environment` object roots."
 
   ARGS:
     ...inputs : String; "The .olean files to parse."
