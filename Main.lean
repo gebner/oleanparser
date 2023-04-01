aad7d6ba982ce0f39b2cfc01a37f54c10692057b
Sebastian Ullrich <sebasti@nullri.ch>
2023-04-01 18:51:16 +0200

    Use lean4-cli.

--- Main.lean
+++ Main.lean
@@ -1,12 +1,27 @@
+import Cli
 import Oleanparser
 
 open IO System Process
+open Cli
 
-unsafe def main (args : List String) : IO Unit :=
-  if h : args = [] then do
-    println "Usage: ... filename"
-    exit 1
-  else do
-    let cnt ← IO.FS.readBinFile (args.head ‹_›)
-    let (obj, last) ← ofExcept <| parseOLean.run cnt
-    println <| repr obj
+unsafe def runCmd (p : Parsed) : IO UInt32 := do
+  for file in p.variableArgsAs! String do
+    let cnt ← IO.FS.readBinFile file
+    let (obj, _) ← ofExcept <| parseOLean.run cnt
+    if p.hasFlag "dump" then
+      println <| repr obj
+  return 0
+
+unsafe def cmd : Cmd := `[Cli|
+  oleanparser VIA runCmd;
+  "Parses .olean Lean object files and prints various information."
+
+  FLAGS:
+    dump;  "Prints pretty-printed object trees."
+
+  ARGS:
+    ...inputs : String; "The .olean files to parse."
+]
+
+unsafe def main (args : List String) : IO UInt32 :=
+  cmd.validate args
--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,9 @@ open Lake DSL
 
 package oleanparser
 
+require Cli from git "https://github.com/mhuisi/lean4-cli.git" @ "nightly"
+require std from git "https://github.com/leanprover/std4.git"
+
 lean_lib Oleanparser
 
 @[default_target]
